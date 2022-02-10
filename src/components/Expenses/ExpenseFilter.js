f8602dc40f7540d9156dfd15e495771c7f167777
Amen-Musingarimi <takudzwamusinga@gmail.com>
2022-02-10 09:08:08 +0200

    Expense Filter Added

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -1,13 +1,17 @@
 import React from "react";
 
-import "./ExpensesFilter.css";
+import "./ExpenseFilter.css";
 
-const ExpensesFilter = () => {
+const ExpenseFilter = (props) => {
+  const dropdownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
+  };
+
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
@@ -18,4 +22,4 @@ const ExpensesFilter = () => {
   );
 };
 
-export default ExpensesFilter;
+export default ExpenseFilter;
